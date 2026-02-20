81359dc5db0b416861f5b01defb97a0ae600ad51
Keerthi-Reddy12 <bommanakeerthimanoharreddy@gmail.com>
2026-02-20 19:15:10 +0530

    python function

---
+++ function.py
@@ -0,0 +1,11 @@
+def add (a,b):
+    return a+b
+def sub (a,b):
+    return a-b
+def mult (a,b):
+    return a*b
+x=int(input("enter value for x: "))
+y=int(input("enter value fro y: "))
+print("addition: ",add(x,y))
+print("subtration: ",sub(x,y))
+print("product: ",mult(x,y))
